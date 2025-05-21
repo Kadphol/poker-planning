cc03d4ffcfa0123b51ef7fda4525a71a4b94d4e4
Mark-Kadphol <kadphol.l@7solutions.co.th>
2025-05-21 15:29:56 +0700

    chore: format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { goto } from '$app/navigation'
+	let nickname = ''
+
+	function joinRoom() {
+		if (nickname.trim()) {
+			sessionStorage.setItem('nickname', nickname)
+			goto('/room/' + crypto.randomUUID())
+		}
+	}
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center gap-4">
+	<h1 class="text-3xl font-bold">Planning Poker</h1>
+	<input type="text" bind:value={nickname} placeholder="Enter nickname" class="input" />
+	<button on:click={joinRoom} class="rounded bg-blue-600 px-4 py-2 text-white">Create Room</button>
+</div>
+
+<style>
+	.input {
+		@apply w-64 rounded border p-2;
+	}
+</style>
